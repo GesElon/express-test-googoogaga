92058983b2b3482bfc133de2e9742586704345a3
GesElon <khaijingvouy@gmail.com>
2026-02-08 21:24:32 +0700

    Yea, I forgot cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello!');
